cdde8cb68a09b3ae14c16068960694e2780567d9
pipeht23111 <pipeht2311@gmail.com>
2026-02-01 16:58:04 -0500

    modified:   Talleres/Taller 2/taller2-daniel/taller2.py

--- Talleres/Taller 2/taller2-daniel/taller2.py
+++ Talleres/Taller 2/taller2-daniel/taller2.py
@@ -3,3 +3,6 @@ print("Taller 2 and Hello world")
 print("Daniel Benavides, taller 2 de ACTD, literal 1.10")
 
 amogus = 10
+
+print("Soy andres y estoy editando el archivo de danielinho")
+print("daniel no ha hecho nada")
