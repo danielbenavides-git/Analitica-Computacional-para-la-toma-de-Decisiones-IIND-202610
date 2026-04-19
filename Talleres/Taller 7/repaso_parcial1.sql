79942b6c7e17f5e6acf8736e7b85845064e16b6a
Daniel Benavides <d.benavidess@uniandes.edu.co>
2026-04-19 13:41:31 -0500

    modified:   Talleres/Taller 7/repaso_parcial1.sql

--- Talleres/Taller 7/repaso_parcial1.sql
+++ Talleres/Taller 7/repaso_parcial1.sql
@@ -1,3 +1,7 @@
+"1)	Liste el salario promedio de los profesores de cada unidad académica. 
+La relación resultada debe tener 2 atributos con nombres ’nombre unid’ y 
+’prom salario’, en ese orden."
+
 SELECT nombre_unid, AVG(salario) AS prom_salario
 FROM instructor
 GROUP BY nombre_unid;
@@ -6,12 +10,15 @@ GROUP BY nombre_unid;
 Filtre sus resultados para incluir solamente las unidades que ofrezcan cursos con 
 un promedio de máximo 4 créditos."
 
-SELECT AVG(creditos) AS creditos_promedio
+SELECT nombre_unid, AVG(creditos) AS prom_creditos
 FROM curso
-WHERE id_unid IN (
-    SELECT id_unid
-    FROM curso
-    GROUP BY id_unid
-    HAVING AVG(creditos) <= 4
-);
+GROUP BY nombre_unid
+HAVING AVG(creditos) <= 4;
 
+"3)	Liste todos los profesores (nombres) que dictan un curso con la palabra modelo en su nombre."
+
+SELECT i.nombre
+FROM instructor AS i, dicta AS d, curso AS c
+JOIN d ON i.inst_ID = d.inst_ID
+JOIN c ON d.curso_cod = c.curso_cod
+WHERE c.nombre LIKE '%modelo%';
